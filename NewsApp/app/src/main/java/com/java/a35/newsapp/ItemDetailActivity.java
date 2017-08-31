e1a6d55fcc19bff9f9c4bce3e87fbdbb73339a94
otowakotori <xzh15@mails.tsinghua.edu.cn>
2017-08-31 22:01:40 +0800

    Share action

--- NewsApp/app/src/main/java/com/java/a35/newsapp/ItemDetailActivity.java
+++ NewsApp/app/src/main/java/com/java/a35/newsapp/ItemDetailActivity.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -26,7 +30,7 @@ import android.widget.Toast;
  * in a {@link ItemListActivity}.
  */
 public class ItemDetailActivity extends AppCompatActivity {
-
+    private ShareActionProvider mShareActionProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -126,6 +130,19 @@ public class ItemDetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+        MenuItem item = menu.findItem(R.id.app_bar_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        String title = getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID);
+        CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        if (appBarLayout != null){
+            CharSequence titleName = appBarLayout.getTitle();
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, titleName);
+            shareIntent.setType("text/plain");
+            mShareActionProvider.setShareIntent(shareIntent);
+            Log.d("DEBUG", "title name = " + titleName);
+        }
         return true;
     }
 
